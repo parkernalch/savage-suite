2246872741e3b496318ac0e46024a4f26ca22b46
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-07-17 14:24:06 -0700

    character modal is just an edit window right now with two-way data bindings

--- src/app/components/character-modal/character-modal.component.ts
+++ src/app/components/character-modal/character-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import SavageCharacter from 'src/app/models/Character';
 import _Race from 'src/app/models/Race';
 
@@ -9,20 +9,12 @@ import _Race from 'src/app/models/Race';
 })
 export class CharacterModalComponent implements OnInit {
   isVisible:boolean = false;
-  characterData:SavageCharacter;
+  @Input() characterData: SavageCharacter;
 
   constructor() {
   }
 
   ngOnInit() {
-    let Human: _Race = {
-      name: 'Human',
-      description: "Classic fantasy human",
-      features: []
-    }
-
-    let C = new SavageCharacter('Hector Barrelwaite', Human);
-    this.characterData = C;
   }
 
 }
