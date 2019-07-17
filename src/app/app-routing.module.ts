f353190f6a3de46a2fd28dc87125393e00c7ba7c
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-07-16 18:29:50 -0700

    added basic routing between the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitiativeComponent } from './components/initiative/initiative.component';
+import { CampaignManagerComponent } from './components/campaign-manager/campaign-manager.component';
+import { EncounterBuilderComponent } from './components/encounter-builder/encounter-builder.component';
+import { CharacterVaultComponent } from './components/character-vault/character-vault.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'initiative', component:InitiativeComponent},
+  {path: 'campaign', component:CampaignManagerComponent},
+  {path: 'encounter', component: EncounterBuilderComponent},
+  {path: 'characters', component: CharacterVaultComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
