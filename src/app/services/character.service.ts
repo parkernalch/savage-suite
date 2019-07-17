eb15c53d20c30eca3ebb81f30f3bacfe937309d3
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-07-17 10:56:15 -0700

    started adding services

---
+++ src/app/services/character.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import SavageCharacter, {_SavageCharacter} from '../models/Character';
+import _Race from '../models/Race';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+  characters:SavageCharacter[] = [];
+
+  constructor() { 
+    let Human:_Race = {
+      name: 'Human',
+      description: 'Classic humans',
+      features: []
+    }
+
+    for(let i=0; i<10; i++){
+      let C:SavageCharacter = new SavageCharacter(`Character${i}`, Human);
+      this.characters.push(C);
+    }
+  }
+
+  getCharacters():Observable<SavageCharacter[]>{
+    return of(this.characters);
+  }
+}
