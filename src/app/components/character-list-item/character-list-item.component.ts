458d16c3973926fc994eaaaf9962761ddd3c0c02
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-07-17 14:22:56 -0700

    added the char list item component for the vault

---
+++ src/app/components/character-list-item/character-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import SavageCharacter from 'src/app/models/Character';
+
+@Component({
+  selector: 'app-character-list-item',
+  templateUrl: './character-list-item.component.html',
+  styleUrls: ['./character-list-item.component.sass']
+})
+export class CharacterListItemComponent implements OnInit {
+  isEdit: boolean;
+  @Input() character: SavageCharacter;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleEdit(): void {
+    this.isEdit = !this.isEdit;
+  }
+
+}
