7a6ba1eefcea7e44970522fb444efc948fdc07d3
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-07-17 11:09:20 -0700

    added header and footer, and a handful more components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { CampaignManagerComponent } from './components/campaign-manager/campaign
 import { EncounterBuilderComponent } from './components/encounter-builder/encounter-builder.component';
 import { CharacterVaultComponent } from './components/character-vault/character-vault.component';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 
 const routes: Routes = [
+  {path: '', component:HomepageComponent},
   {path: 'initiative', component:InitiativeComponent},
   {path: 'campaign', component:CampaignManagerComponent},
   {path: 'encounter', component: EncounterBuilderComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CampaignManagerComponent } from './components/campaign-manager/campaign
 import { EncounterBuilderComponent } from './components/encounter-builder/encounter-builder.component';
 import { InitiativeComponent } from './components/initiative/initiative.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HeaderComponent } from './components/layout/header/header.component';
     CampaignManagerComponent,
     EncounterBuilderComponent,
     InitiativeComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
