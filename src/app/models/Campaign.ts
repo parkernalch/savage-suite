3a10047f7c4ab443cd62d930f66af5af35171a70
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-07-23 18:08:05 -0700

    added more information to the campaign model for later display

--- src/app/models/Campaign.ts
+++ src/app/models/Campaign.ts
@@ -1,3 +1,14 @@
-interface Campaign {
+import { _SavageCharacter } from "./Character";
+import  _ChroniclePost from "./ChroniclePost";
 
+interface _Campaign {
+    id:number;
+    chronicle:_ChroniclePost[];
+    party: _SavageCharacter[];
+    sessions: Date[];
+    next_session: Date[];
+    motd: string;
+    chat: _ChatMessage[];
+
+    
 }
