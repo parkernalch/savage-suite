0a55a7ffc9976545f4198dfd51bece0c8c57ca14
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-08-03 22:32:04 -0700

    started building the encounter builder which will lead into the initiative tracker

--- src/app/components/encounter-builder/encounter-builder.component.ts
+++ src/app/components/encounter-builder/encounter-builder.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { _SavageCharacter } from 'src/app/models/Character';
+import Encounter from 'src/app/models/Encounter';
+import _Campaign from 'src/app/models/Campaign';
+import { CampaignService } from 'src/app/services/campaign.service';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-encounter-builder',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./encounter-builder.component.sass']
 })
 export class EncounterBuilderComponent implements OnInit {
+  combatants: _SavageCharacter[];
+  encounter: Encounter;
+  campaigns: _Campaign[];
+  selectedCampaign: _Campaign;
 
-  constructor() { }
+  // encounterService can load saved instances
+  constructor(
+    private campaignService: CampaignService,
+    private characterService: CharacterService) { }
 
   ngOnInit() {
+    this.campaignService.getCampaigns()
+      .subscribe(campaigns => {
+        this.campaigns = campaigns;
+    });
+
+    
   }
 
 }
