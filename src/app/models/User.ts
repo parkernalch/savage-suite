3f8797bd774f17d9cbeaff43bdc5215443a28d36
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-08-11 00:07:22 -0700

    added role and register date, removed id [backend alignment]

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,6 +1,10 @@
 export interface _User {
-    id:number;
+    // id: number;
     name:string;
     email:string;
     username:string;
+    // password:string;
+    // is_verified: boolean;
+    role: string;
+    register_date:Date;
 }
