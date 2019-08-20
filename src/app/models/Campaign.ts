bf11ff69ca5a3d437780db18f307152d1e85d737
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-08-19 17:27:35 -0700

    added custom content to the campaign for homebrew/modules

--- src/app/models/Campaign.ts
+++ src/app/models/Campaign.ts
@@ -1,6 +1,10 @@
 import SavageCharacter, { _SavageCharacter } from "./Character";
 import  _ChroniclePost from "./ChroniclePost";
 import _ChatMessage from "./ChatMessage";
+import _Edge from './Edge';
+import _Hindrance from './Hindrance';
+import _Power from './Power';
+import _Race from './Race';
 
 export default interface _Campaign {
     id:string;
@@ -12,4 +16,10 @@ export default interface _Campaign {
     next_session: Date;
     motd: string;
     chat: _ChatMessage[];
+    custom_content?: {
+        edges: _Edge[];
+        hindranes: _Hindrance[];
+        powers: _Power[];
+        races: _Race[];
+    }
 }
