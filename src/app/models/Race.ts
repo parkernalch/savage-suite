f48caaf961c7dd623cc49ac878e98b8edd1e8534
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-08-22 11:58:27 -0700

    added hindrances field to Race

--- src/app/models/Race.ts
+++ src/app/models/Race.ts
@@ -1,10 +1,12 @@
 import _Edge from './Edge';
+import _Hindrance from './Hindrance';
 
 interface _Race {
     id: string;
     name:string;
     description:string;
     features: _Edge[];
+    hindrances: _Hindrance[];
     base_stats:{
         agility: number;
         smarts: number;
